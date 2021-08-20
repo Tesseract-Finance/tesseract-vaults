13434ad291a933c97407e230483a43da74fedd6d
andrejrakic <andrej.rakic@mvpworkshop.co>
2021-08-20 10:26:59 +0200

    Add contracts for the Gelato

---
+++ contracts/StrategyFacade.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GNU Affero
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+
+import { StrategyAPI } from "./BaseStrategy.sol";
+
+/// @title Facade contract for Gelato Resolver contract
+/// @author Tesseract Finance
+contract StrategyFacade {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    
+    EnumerableSet.AddressSet internal availableStrategies;
+    address public immutable multiSig;
+    
+    event StrategyAdded(address strategy);
+    event StrategyRemoved(address strategy);
+
+    modifier onlyMultiSig {
+      require(msg.sender == multiSig, "Only MultiSig can call");
+      _;
+    }
+    
+    constructor(address _multiSig) public {
+        multiSig = _multiSig;
+    }
+    
+    function addStrategy(address _strategy) public onlyMultiSig {
+        require(!availableStrategies.contains(_strategy), "StrategyResolver::addStrategy: Strategy already added");
+        
+        availableStrategies.add(_strategy);
+        
+        emit StrategyAdded(_strategy);
+    }
+    
+    function removeStrategy(address _strategy) public onlyMultiSig {
+        require(!!availableStrategies.contains(_strategy), "StrategyResolver::removeStrategy: Strategy already removed");
+        
+        availableStrategies.remove(_strategy);
+        
+        emit StrategyRemoved(_strategy);
+    }
+    
+    function checkHarvest(uint256 _callCost) public view returns(bool canExec, address strategy) {
+        for (uint256 i; i < availableStrategies.length(); i++) {
+            address currentStrategy = availableStrategies.at(i);
+            if(StrategyAPI(currentStrategy).harvestTrigger(_callCost)) {
+                return(canExec = true, strategy = currentStrategy);
+            }
+        }
+        
+        return(canExec = true, strategy = address(0));
+    }
+    
+    function harvest(address _strategy) public {
+        StrategyAPI(_strategy).harvest();
+    }
+    
+}
---
+++ contracts/StrategyResolver.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GNU Affero
+pragma solidity ^0.6.0;
+
+/// @title Functions from the Facade which Resolver needs to call
+interface IStrategyFacade {
+    function checkHarvest(uint256 _callCost) external view returns(bool canExec, address strategy);
+    function harvest(address _strategy) external;
+}
+
+/// @title Resolver contract for Gelato harvest bot
+/// @author Tesseract Finance
+contract StrategyResolver {
+    
+    address public immutable multiSig;
+    address public facade;
+    uint256 public checkInterval;
+    uint256 public lastExecuted;
+    
+    event CheckIntervalUpdated(uint256 checkInterval);
+
+    modifier onlyMultiSig {
+      require(msg.sender == multiSig, "Only MultiSig can call");
+      _;
+    }
+    
+    constructor(address _multiSig, address _facade, uint256 _checkInterval) public {
+        multiSig = _multiSig;
+        facade = _facade;
+        checkInterval = _checkInterval;
+        lastExecuted = block.timestamp;
+    }
+    
+    function setCheckInterval(uint256 _checkInterval) public onlyMultiSig {
+        checkInterval = _checkInterval;
+        
+        emit CheckIntervalUpdated(_checkInterval);
+    }
+    
+    function check(uint256 _callCost) external view returns(bool canExec, bytes memory execPayload) {
+        require((block.timestamp - lastExecuted) > checkInterval, "StrategyResolver::check: Too early to execute action smart contract");
+        
+        (bool _canExec, address _strategy) = IStrategyFacade(facade).checkHarvest(_callCost);
+        
+        canExec = _canExec;
+
+        execPayload = abi.encodeWithSelector(
+            IStrategyFacade.harvest.selector,
+            address(_strategy)
+        );
+    }
+}

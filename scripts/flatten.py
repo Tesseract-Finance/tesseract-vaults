c641ae721569532d66f601253420319ec563bd67
Filip Pajic <fica.pajic@gmail.com>
2021-10-10 21:33:27 +0200

    Updated licence

--- scripts/flatten.py
+++
@@ -1,8 +0,0 @@
-from brownie import StrategyGenLevAAVE
-
-
-def main():
-    source = StrategyGenLevAAVE.get_verification_info()["flattened_source"]
-
-    with open("flat.sol", "w") as f:
-        f.write(source)

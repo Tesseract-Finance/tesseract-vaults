1a8186098885c7437a2636f6944780a54bcc7718
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2021-10-13 21:24:22 +0200

    Leverage AAVE Strategy upgrade
    * Upgraded lev AAVE strategy
    
    * Linting fixes
    
    * Updated licence
    
    * Removed router setter

---
+++ contracts/interfaces/aave/IPriceOracle.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity >=0.6.0 <0.7.0;
+
+interface IPriceOracle {
+    function getAssetPrice(address _asset) external view returns (uint256);
+
+    function getAssetsPrices(address[] calldata _assets) external view returns (uint256[] memory);
+
+    function getSourceOfAsset(address _asset) external view returns (address);
+
+    function getFallbackOracle() external view returns (address);
+}
